cdfcf9d5f497d932dd5a6318fefea5f378005c7f
Nikki679 <nikkikumari679@gmail.com>
2021-01-05 11:40:29 +0530

    store user data in user area

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header=({ startLogout })=>(
     <div>
         <header>
             <h1>Expensify</h1>
-            <NavLink activeClassName='is-active' to='/' exact={true}>Dashboard</NavLink>
+            <NavLink activeClassName='is-active' to='/dashboard'>Dashboard</NavLink>
             <NavLink activeClassName='is-active' to='/create'>Create Expense</NavLink>
             <button onClick={startLogout}>Logout</button>
         </header>
